609d93e7844e8b600feacddaf6e370ad06f590bf
desh01prajakta <prajudeshmukh11@gmail.com>
2024-02-22 21:45:43 -0600

    adds value

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 function Register() {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+  });
   const eventHandler = (event) => {
     event.preventDefault();
+  };
+
+  const onUserInput = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
   return (
     <div>
@@ -8,23 +21,53 @@ function Register() {
       <form onSubmit={eventHandler}>
         <label>
           Username
-          <input type="text" placeholder="username" />
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            value={userInfo.username}
+            onChange={onUserInput}
+          />
         </label>
         <label>
           Password
-          <input name="password" type="password" placeholder="password" />
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            value={userInfo.password}
+            onChange={onUserInput}
+          />
         </label>
         <label>
           Email
-          <input type="text" placeholder="email" />
+          <input
+            type="text"
+            placeholder="email"
+            name="email"
+            value={userInfo.email}
+            onChange={onUserInput}
+          />
         </label>
         <label>
           First Name
-          <input type="text" placeholder="first_name" />
+          <input
+            type="text"
+            placeholder="first_name"
+            name="first_name"
+            value={userInfo.first_name}
+            onChange={onUserInput}
+          />
         </label>
         <label>
           Last Name
-          <input type="text" placeholder="lat_name" />
+          <input
+            type="text"
+            placeholder="last_name"
+            name="last_name"
+            value={userInfo.last_name}
+            onChange={onUserInput}
+          />
         </label>
         <button>Submit</button>
       </form>
