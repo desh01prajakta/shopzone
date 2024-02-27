6257a4dfa543b2282d3a640c58ca4d21b72d6e32
desh01prajakta <prajudeshmukh11@gmail.com>
2024-02-26 23:35:21 -0600

    productdetails component done

--- src/component/ProductDetails.jsx
+++ src/component/ProductDetails.jsx
@@ -1,32 +1,36 @@
 import { useProductDetailsQuery } from "../redux/api";
 import { useParams } from "react-router-dom";
 
+function ProductDetails() {
+  let { id } = useParams();
+  const { data, error, isLoading } = useProductDetailsQuery(id);
 
-function ProductDetails(){
-    let { id } = useParams();
-    const{data, error, isLoading} = useProductDetailsQuery(id);
+  if (isLoading) {
+    return <p>Data is Loading!</p>;
+  }
 
-    if (isLoading){
-        return <p>Data is Loading!</p>
-    };
+  if (error) {
+    return <p>Something Went Wrong!</p>;
+  }
 
-    if(error){
-        return <p>Something Went Wrong!</p>
-    };
+  console.log("data", data);
+  console.log("error", error);
+  console.log("isLoading", isLoading);
 
-    console.log("data", data);
-    console.log("error", error);
-    console.log("isLoading", isLoading)
-
-    return(
-        <div>
-            <h2>Product Details</h2>
-            <img src={product.image} />
-            <p>Price: {product.price}</p>
-            <p> Description: {product.description} </p>
-            <p>Category: {product.category}</p>
-            <p>Rating: {product.rating.rate} ({product.rating.count})</p>
-            </div>
-        );
-      }
+  return (
+    <div>
+      <h2>Product Details</h2>
+      
+          <div>
+            <img src={data.image} />
+            <p>Price: {data.price}</p>
+            <p>Description: {data.description} </p>
+            <p>Category: {data.category}</p>
+            <p>
+              Rating: {data.rating.rate} ({data.rating.count})
+            </p>
+          </div>
+          </div>
+  );
+}
 export default ProductDetails;
