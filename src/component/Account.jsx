3a79bc4230c60cec6e9d70a4c6dcf6a7a467fab2
desh01prajakta <prajudeshmukh11@gmail.com>
2024-02-26 18:00:08 -0600

    finished Account component

--- src/component/Account.jsx
+++ src/component/Account.jsx
@@ -1,32 +1,37 @@
 import { useAccountQuery } from "../redux/api";
 
+function Account(props) {
+  const { data, error, isLoading } = useAccountQuery(props);
+  console.log(props);
+  console.log("DATA", data);
+  console.log("ERROR", error);
+  console.log("isLoading", isLoading);
 
-function Account (props){
-    const {data, error, isLoading} = useAccountQuery();
-    console.log(props);
-    console.log("DATA", data);
-    console.log("ERROR", error);
-    console.log("isLoading", isLoading)
+  if (error || (!data?.user && isLoading)) {
+    return <p>Something went wrong!</p>;
+  }
 
-    if (error || (!data?.user && isLoading)) {
-        return <p>Something went wrong!</p>;
-      }
-    
-      if (isLoading) {
-        return <p>Loading...</p>;
-      }
-return(
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
     <section>
-        <h2>Account</h2>
+      <h2>Account</h2>
+      {data?.user && (
         <ul>
-            <li>Username: {data.user.username}</li>
-            {data.user.email ? <li>Email: {data.user.email}</li> : ""}
-            {data.user.first_name ? <li>First Name: {data.user.first_name}</li> : ("")}
-            {data.user.last_name ? <li>Last Name: {data.user.last_name}</li> : ("")}
-            {data.user.phone ? <li>Phone: {data.user.phone}</li> : ("")}
-            {data.user.address ? <li>Address: {data.user.address}</li> : ("")}
-        </ul>        
+          <li>Username: {data.user.username}</li>
+          {data.user.email ? <li>Email: {data.user.email}</li> : ""}
+          {data.user.first_name ? (
+            <li>First Name: {data.user.first_name}</li>
+          ) : (
+            ""
+          )}
+          {data.user.last_name ? <li>Last Name: {data.user.last_name}</li> : ""}
+          {data.user.phone ? <li>Phone: {data.user.phone}</li> : ""}
+          {data.user.address ? <li>Address: {data.user.address}</li> : ""}
+        </ul>
+      )}
     </section>
-)
+  );
 }
 export default Account;
